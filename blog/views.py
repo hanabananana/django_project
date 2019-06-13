8dacbc5016c5cb0d15d4093ac7f961377c476fbb
hana <kimhana0525@naver.com>
2019-06-14 00:12:27 +0900

    added html template!

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+	return render(request, 'blog/post_list.html', {})
+
+
+'''
+post_list함수는 request를 넘겨받아 render메소드를 호출한다.
+render메소가 return한 blog/post_list.html템플릿을 보여준다.
+'''
