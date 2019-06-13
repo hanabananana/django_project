3c640fc361fedca1b616b01f8a8e93e914db152a
hana <kimhana0525@naver.com>
2019-06-14 02:08:28 +0900

    added view and template for detailed blog post with css for the site

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,7 @@
-# blog 폴더내에 urls.py라는 파일을 생성하고 코드를 넣음
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+# view에 매개변수 pk(primary key)를 추가하자
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
